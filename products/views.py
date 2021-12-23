245924d26ecad89990b2927d1befa5cdff094ce5
solsolsol11 <ushio941@gmail.com>
2021-12-24 03:04:06 +0900

    질문등록까지 구현 완료

---
+++ products/views.py
@@ -0,0 +1,24 @@
+
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from products.models import Product
+
+
+def product_list(request: HttpRequest):
+    products = Product.objects.order_by('-id')
+
+    return render(request, "products/product_list.html", {
+        "products": products
+    })
+
+def product_detail(request: HttpRequest, id):
+    product = get_object_or_404(Product, id=id)
+
+    product_reals = product.product_reals.order_by('option_1_display_name', 'option_2_display_name')
+
+
+    return render(request, "products/product_detail.html", {
+        "product": product,
+        "product_reals": product_reals,
+    })
---
+++ qna/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from qna import views
+
+app_name = 'qna'
+
+urlpatterns =[
+    path('create_question/<str:content_type>/<int:object_id>/', views.create_question, name= 'create_question')
+]
