4e52993297406c562e3bb9c0438ba9462c5e7d91
solsolsol11 <ushio941@gmail.com>
2021-12-24 09:30:46 +0900

    리다이렉트

--- qna/views.py
+++ qna/views.py
@@ -1,6 +1,6 @@
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from products.models import Product
@@ -15,4 +15,5 @@ def create_question(request: HttpRequest, content_type, object_id):
         body = request.POST.get("body")
         question = Question(user_id=1, content_type=product_content_type, object_id=product_id, body=body)
         question.save()
+        return redirect("products:detail", id=product_id)
     return HttpResponse("성공")
