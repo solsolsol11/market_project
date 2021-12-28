e5f4060b15d676229b8ee45f7f32b457edc24aa9
solsolsol11 <ushio941@gmail.com>
2021-12-28 12:26:49 +0900

    오류 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from accounts import views
 
+app_name = 'accounts'
+
 urlpatterns = [
-    path('signup/', views.signup),
-    path('signout/', views.signout),
-    path('signin/', views.signin),
+    path('signup/', views.signup, name ='signup'),
+    path('signout/', views.signout, name ='signout'),
+    path('signin/', views.signin, name ='signin'),
 ]
