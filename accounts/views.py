8facdf48d8f53b113e91e7aa6c113a36efe3fb38
solsolsol11 <ushio941@gmail.com>
2021-12-29 10:45:16 +0900

    @logout_required 적용

--- accounts/views.py
+++ accounts/views.py
@@ -3,18 +3,22 @@ from django.contrib.auth import login as auth_login
 from django.contrib.auth.views import logout_then_login, LoginView
 from django.http import HttpRequest
 from django.shortcuts import render, redirect
-
+from .decorators import logout_required
 # Create your views here.
 from .forms import SignupForm
 
-signin = LoginView.as_view(template_name="accounts/signin.html")
+
+@logout_required
+def signin(request:HttpRequest):
+    return LoginView.as_view(template_name="accounts/signin.html")(request)
+
 
 
 def signout(request:HttpRequest):
     messages.success(request, "로그아웃 되었습니다.")
     return logout_then_login(request)
 
-
+@logout_required
 def signup(request:HttpRequest):
     if request.method == 'POST':
         form = SignupForm(request.POST)
