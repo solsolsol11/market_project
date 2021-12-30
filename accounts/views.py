f40d008e54c0ed5f1ac1a70a60486c2b761c0741
solsolsol11 <ushio941@gmail.com>
2021-12-30 13:30:23 +0900

    카카오 로그인테스트

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,13 @@
+import requests
 from django.contrib import messages
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.views import logout_then_login, LoginView
 from django.contrib.messages.views import SuccessMessageMixin
+
+
 from django.db.models import QuerySet
 from django.forms import ModelForm
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from lazy_string import LazyString
@@ -80,3 +83,46 @@ def find_username(request: HttpRequest):
     return render(request, 'accounts/find_username.html', {
         'form': form,
     })
+
+
+def kakao_login(request: HttpRequest):
+    client_id = "aada477afc042d55e52f3b009de41e5b"
+    REDIRECT_URI = "http://localhost:8000/accounts/signin/kakao/callback"
+    return redirect(
+        f"https://kauth.kakao.com/oauth/authorize?client_id={client_id}&redirect_uri={REDIRECT_URI}&response_type=code"
+    )
+
+
+class KakaoException(Exception):
+    def __init__(self):
+        super().__init__('에러메시지')
+
+
+def kakao_signin_callback(request):
+    # (1)
+    code = request.GET.get("code")
+    client_id = "aada477afc042d55e52f3b009de41e5b"
+    REDIRECT_URI = "http://localhost:8000/accounts/signin/kakao/callback"
+
+    # (2)
+    token_request = requests.get(
+        f"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id={client_id}&redirect_uri={REDIRECT_URI}&code={code}"
+    )
+    # (3)
+
+    token_json = token_request.json()
+
+    error = token_json.get("error", None)
+    if error is not None:
+        raise KakaoException()
+
+    access_token = token_json.get("access_token")
+
+    profile_request = requests.get(
+        "https://kapi.kakao.com/v2/user/me",
+        headers={"Authorization": f"Bearer {access_token}"},
+    )
+    profile_json = profile_request.json()
+
+    id = profile_json.get("id")
+
